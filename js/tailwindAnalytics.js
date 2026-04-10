3d2b157122fa672c42cc816fdbe3b71b6eb71f82
Marc Ace J. Legaspi <90955780+mecsung@users.noreply.github.com>
2026-04-10 17:45:35 +0800

    Update
    Structured CSS

---
+++ js/tailwindAnalytics.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+    extend: {
+        colors: {
+        primary: '#15803D',
+        'primary-dark': '#166534',
+        'primary-light': '#22c55e',
+        'primary-bg': '#f0fdf4'
+        }
+    }
+    }
+}
